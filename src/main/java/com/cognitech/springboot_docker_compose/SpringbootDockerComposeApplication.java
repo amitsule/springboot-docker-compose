bf0a5d0907c16bd312c80178fb053373a02dd1b6
Amit Sule <amit.sule@gmail.com>
2025-08-28 11:10:11 +0100

    Added JdbcConnectionDetails to view connection string

--- src/main/java/com/cognitech/springboot_docker_compose/SpringbootDockerComposeApplication.java
+++ src/main/java/com/cognitech/springboot_docker_compose/SpringbootDockerComposeApplication.java
@@ -1,7 +1,11 @@
 package com.cognitech.springboot_docker_compose;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class SpringbootDockerComposeApplication {
@@ -9,5 +13,14 @@ public class SpringbootDockerComposeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerComposeApplication.class, args);
 	}
+
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcConnectionDetails jdbc)
+    {
+        return args -> {
+            String details = "class: " + jdbc.getDriverClassName() + "\nurl: " + jdbc.getJdbcUrl();
+            System.out.println(details);
+        };
+    }
 
 }
